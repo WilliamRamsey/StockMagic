77f9bdedb8c08c948572bd1a099ec7347a44bdef
WilliamRamsey <williamdawsonramsey@gmail.com>
2023-06-28 19:22:32 -0400

    Database Refactor

--- StockMagic/__init__.py
+++ StockMagic/__init__.py
@@ -44,62 +44,99 @@ class Alpaca:
 
             end = dt.datetime.now()
             start = end - dt.timedelta(minutes=time_dif)
-        print(start.strftime("%Y-%m-%d"))
         params = StockBarsRequest(symbol_or_symbols=symbol,
                                   timeframe=frequency,
                                   start=start,
                                   end=end)
         return self.market_client.get_stock_bars(params).df
 
-account = Alpaca("PKKYD9REBHS0NIZILM0K", "6YFj01fzxhMT23jH5R8Y7cSmObOUYgfrohQ3NK4D", True, True)
-print(account.historical_data("AAPL", "Day", bars=3))
-# print(account.order("AAPL", "buy", 1))
-
-
-# Datastructure
-# database
-# -
 class DataBase:
     def __init__(self):
         # Established local directory for storing data
         self.rootdir = os.getcwd()
         if not os.path.exists(self.rootdir + "\\database"):
             os.makedirs(self.rootdir + "\\database")
-        if not os.path.exists(self.rootdir + "/database\\raw"):
-            os.makedirs(self.rootdir + "\\database\\raw")
-        if not os.path.exists(self.rootdir + "\\database\\normalized"):
-            os.makedirs(self.rootdir + "\\database\\normalized")
-        if not os.path.exists(self.rootdir + "\\database\\raw\\Minute"):
-            os.makedirs(self.rootdir + "\\database\\raw\\Minute")
-        if not os.path.exists(self.rootdir + "\\database\\raw/Hour"):
-            os.makedirs(self.rootdir + "\\database\\raw\\Hour")
-        if not os.path.exists(self.rootdir + "\\database\\raw\\Day"):
-            os.makedirs(self.rootdir + "\\database\\raw\\Day")
-        if not os.path.exists(self.rootdir + "\\database\\normalized\\Minute"):
-            os.makedirs(self.rootdir + "\\database\\normalized\\Minute")
-        if not os.path.exists(self.rootdir + "\\database\\normalized\\Hour"):
-            os.makedirs(self.rootdir + "\\database\\normalized\\Hour")
-        if not os.path.exists(self.rootdir + "\\database\\normalized\\Day"):
-            os.makedirs(self.rootdir + "\\database\\normalized\\Day")
+        if not os.path.exists(self.rootdir + "\\database\\securities"):
+            os.makedirs(self.rootdir + "\\database\\securities")
+        if not os.path.exists(self.rootdir + "\\database\\datasets"):
+            os.makedirs(self.rootdir + "\\database\\datasets")
 
-    def reset_database(self):
+
+
+    def reset(self):
         # Deletes all files in the database
-        for dir in os.listdir(self.rootdir + "\\database\\raw"):
-            for file in os.listdir(self.rootdir + "\\database\\raw\\" + dir):
-                open(f"{self.rootdir}\\database\\raw\\{dir}\\{file}", "w").close()
+        for dir in os.listdir(self.rootdir + "\\database\\securities"):
+            for file in os.listdir(self.rootdir + "\\database\\securities\\" + dir):
+                os.remove(f"{self.rootdir}\\database\\securities\\{dir}\\{file}")
             
-        for dir in os.listdir(self.rootdir + "\\database\\normalized"):
-            for file in os.listdir(self.rootdir + "\\database\\normalized\\" + dir):
-                open(f"{self.rootdir}\\database\\normalized\\{dir}\\{file}", "w").close()
+        for dir in os.listdir(self.rootdir + "\\database\\datasets"):
+            for file in os.listdir(self.rootdir + "\\database\\datasets\\" + dir):
+                os.remove(f"{self.rootdir}\\database\\datasets\\{dir}\\{file}")
 
-    def add_entry(self, data, symbol, frequency, normalized=False):
-        # Write file in /database/{type}/{frequency}/
-        # Data will be imputted as dataframe and saved to CSV
-        if normalized:
-            data.to_csv(f"{self.rootdir}\\database\\normalized\\{frequency}\\{symbol}.csv")
-        else:
-            data.to_csv(f"{self.rootdir}\\database\\raw\\{frequency}\\{symbol}.csv")
+    class Security:
+        def __init__(self, data=None, path=None, symbol=None, frequency=None):
+            self.rootdir = os.getcwd()
+
+            if data is not None:
+                self.data = data
+                self.symbol = data.index.to_list()[0][0]
+                time1 = data.index.to_list()[0][1]
+                time2 = data.index.to_list()[1][1]
+                timedelta = time2 - time1
+                dif = timedelta.total_seconds() / 60
+
+                if dif == 1:
+                    self.frequency = "Minute"
+                elif dif == 60:
+                    self.frequency = "Hour"
+                else:  
+                    self.frequency = "Day"
+    
+                self.datapath = f"{self.rootdir}\\database\\securities\\{self.symbol}"
+
+                if not os.path.exists(self.datapath):
+                    os.makedirs(self.datapath)
+                if not os.path.exists(f"{self.datapath}\\{self.frequency}"):
+                    os.makedirs(f"{self.datapath}\\{self.frequency}")
+
+        """
+        def __call__(self):
+            return self.data
+        """
+
+        def add(self):
+            # Write file in /database/{type}/{frequency}/
+            # Data will be imputted as dataframe and saved to CSV
+            start = self.data.index.to_list()[0][1]
+            end = self.data.index.to_list()[-1][1]
+            self.time_range = f"{start.strftime('[%Y-%m-%d %H-%M-%S]')} - {end.strftime('[%Y-%m-%d %H-%M-%S]')}"
+            path = f"{self.datapath}/{self.frequency}/{self.time_range}.csv"
+            print(path)
+            self.data.to_csv(path)
+
+        def remove(self):
+            # Deletes file in /database/{type}/{frequency}/
+            os.remove(self.datapath)
+        
+        """
+        def normalize(self, output_length):
+            if len(self.data['close'].to_numpy()) < output_length:
+                return None
+            else:
+                unormalized_data = self.data['close'].to_numpy()[-output_length:]
+                normalized_data = (unormalized_data - unormalized_data.min()) / (unormalized_data.max() - unormalized_data.min())
+                return normalized_data
+        """
+
+    class DataSet:
+        pass
+
+account = Alpaca("PKKYD9REBHS0NIZILM0K", "6YFj01fzxhMT23jH5R8Y7cSmObOUYgfrohQ3NK4D", True, True)
+# print(account.historical_data("AAPL", "Day", bars=3))
+# print(account.order("AAPL", "buy", 1))
 
 base = DataBase()
-base.reset_database()
-base.add_entry(account.historical_data("AAPL", "Minute", bars=3), "AAPL", "Minute", normalized=False)
+datapoint = base.Security(data=account.historical_data("AAPL", "Day", bars=10))
+# print(datapoint.normalize(3))
+datapoint.add()
+# base.reset()
