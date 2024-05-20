951ecebe3c6649e115e240cf30b12a4061c0774e
WilliamRamsey <williamdawsonramsey@gmail.com>
2024-05-19 23:39:26 -0400

    Added options request to app

--- App/PortfolioManager/alpaca_account.py
+++ App/PortfolioManager/alpaca_account.py
@@ -2,8 +2,9 @@ from typing import Any
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
-from alpaca.data.historical import StockHistoricalDataClient
-from alpaca.data.requests import StockBarsRequest
+from alpaca.data.historical.stock import StockHistoricalDataClient
+from alpaca.data.historical.option import OptionHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest, OptionBarsRequest
 from alpaca.data.timeframe import TimeFrame
 import datetime as dt
 
@@ -15,9 +16,14 @@ class Alpaca:
         self.subscribed = subscribed
 
         self.market_client = StockHistoricalDataClient(self.API_KEY, self.SEC_KEY)
+        self.option_client = OptionHistoricalDataClient(self.API_KEY, self.SEC_KEY)
         self.trading_client = TradingClient(self.API_KEY, self.SEC_KEY, paper=True)
         self.account = self.trading_client.get_account()
-    
+
+        print(self.account.options_approved_level)
+        print(self.account.options_trading_level)
+        print(self.account.options_buying_power)
+
     def order(self, symbol, buy, qty):
         if buy:
             side = OrderSide.BUY
@@ -28,7 +34,7 @@ class Alpaca:
         market_order = self.trading_client.submit_order(order_data)
         return market_order
 
-    def historical_data(self, symbol, frequency, start=None, end=None, period=None):
+    def historical_stock_prices(self, symbol, frequency, start=None, end=None, period=None):
         if period is not None:
             if frequency == "Minute":
                 time_dif = period
@@ -48,3 +54,29 @@ class Alpaca:
                                   start=start,
                                   end=end)
         return self.market_client.get_stock_bars(params).df
+    
+    def historical_option_prices(self, symbol, frequency, start=None, end=None, period=None):
+        if period is not None:
+            if frequency == "Minute":
+                time_dif = period
+                frequency = TimeFrame.Minute
+            elif frequency == "Hour":
+                time_dif = 60 * period
+                frequency = TimeFrame.Hour
+            else:
+                time_dif = 1440 * period
+                frequency = TimeFrame.Day
+
+            end = dt.datetime.now()
+            start = end - dt.timedelta(minutes=time_dif)
+            
+        params = OptionBarsRequest(symbol_or_symbols=symbol,
+                                  timeframe=frequency,
+                                  start=start,
+                                  end=end)
+        return self.market_client.get_stock_bars(params).df
+
+
+account = Alpaca("PKQFAGYL283JTWQL7G31", "nf73HbHvmUHnx3oSHdQby0hGFagsOWThXU3XRT91", True, True)
+print(account.historical_option_prices("scwx", "Day", period=10))
+print(account.historical_stock_prices("scwx", "Day", period=10))
